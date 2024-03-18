49d29e10028f92ee71ff61a409e0f3e115d871ee
dkatrodiya17 <142277981+dkatrodiya17@users.noreply.github.com>
2024-03-17 23:52:21 -0400

    Update CandidateActivity.kt

--- app/src/main/java/com/example/groupproject/CandidateActivity.kt
+++ app/src/main/java/com/example/groupproject/CandidateActivity.kt
@@ -1,10 +1,17 @@
 package com.example.groupproject
 
 import CandidateAdapter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 
 class CandidateActivity : AppCompatActivity() {
@@ -12,15 +19,37 @@ class CandidateActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: CandidateAdapter
     private lateinit var databaseReference: DatabaseReference
+    private lateinit var editTextSearch: EditText
+    private lateinit var currentUser: FirebaseUser
+    private lateinit var nodata: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_candidate)
 
         recyclerView = findViewById(R.id.recyclerView2)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        adapter = CandidateAdapter()
-        recyclerView.adapter = adapter
+        editTextSearch = findViewById(R.id.search)
+        nodata = findViewById(R.id.nodata)
+
+
+        val imgbacjk: ImageView = findViewById(R.id.imgbacjk)
+        imgbacjk.setOnClickListener{
+            onBackPressed()
+        }
+
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        editTextSearch.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                adapter.filter.filter(s)
+            }
+        })
+
+        // Get the current user
+        currentUser = FirebaseAuth.getInstance().currentUser ?: return
 
         // Initialize Firebase database reference
         databaseReference = FirebaseDatabase.getInstance().reference.child("candidates")
@@ -36,9 +65,15 @@ class CandidateActivity : AppCompatActivity() {
                 candidatesList.clear()
                 for (postSnapshot in snapshot.children) {
                     val candidate = postSnapshot.getValue(Candidate::class.java)
-                    candidate?.let { candidatesList.add(it) }
+                    candidate?.let {
+                        // Fetch connection status from user-specific data in the database
+                        val isConnected = postSnapshot.child("connections").child(currentUser.uid).exists()
+                        it.isConnected = isConnected
+                        candidatesList.add(it)
+                    }
                 }
-                adapter.submitList(candidatesList)
+                adapter = CandidateAdapter(candidatesList, nodata)
+                recyclerView.adapter = adapter
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -47,6 +82,3 @@ class CandidateActivity : AppCompatActivity() {
         })
     }
 }
-
-
-
